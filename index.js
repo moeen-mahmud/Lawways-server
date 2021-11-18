a0863515feee8dd348197d7cc28945b348d1abb7
Moeen Mahmud <write2fahim007@gmail.com>
2021-11-18 19:23:01 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// Necessary modules
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Testing endpoint
+app.get("/", (req, res) => {
+  res.send("Service Website Is Running 🔥");
+});
+
+app.listen(port, () => {
+  console.log("Server is running at port", port);
+});
