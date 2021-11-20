81f91b1a3d683b8eec74c7e017133002b942e706
Moeen Mahmud <write2fahim007@gmail.com>
2021-11-20 17:13:44 +0600

    added mongo client

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Necessary modules
 const express = require("express");
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
@@ -8,6 +10,34 @@ const port = process.env.PORT || 5000;
 // Middlewares
 app.use(cors());
 app.use(express.json());
+
+// Mongo Client
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@lawwayscluster.hxfz3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Mongo Function
+async function run() {
+  try {
+    await client.connect();
+
+    const database = client.db("lawwaysDatabase");
+    const lawyerCollection = database.collection("lawyers");
+
+    // GET all lawyers
+    app.get("/lawyers", async (req, res) => {
+      const cursor = lawyerCollection.find({});
+      const result = await cursor.toArray();
+      res.json(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
 
 // Testing endpoint
 app.get("/", (req, res) => {
