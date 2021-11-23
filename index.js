d0fd3d745995d2e761fea6705c044ff7a95e2619
Moeen Mahmud <write2fahim007@gmail.com>
2021-11-23 13:46:20 +0600

    implement delete method

--- index.js
+++ index.js
@@ -79,6 +79,14 @@ async function run() {
         res.json(result);
       }
     });
+
+    // DELETE a order
+    app.delete("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
+      res.json(result);
+    });
   } finally {
     // await client.close();
   }
